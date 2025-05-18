ae4f6bc2ceab416a981f7c71034b08413fcf2f46
krunalnagendra <147229701+krunalnagendra@users.noreply.github.com>
2025-05-18 13:00:11 +0530

    Time: 0 ms (100%), Space: 40.7 MB (18.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,6 +3,12 @@ class Solution {
         if(n<=1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int a=0,b=1;
+        for(int i=2; i<=n; i++){
+            int temp = b;
+            b+=a;
+            a=temp;
+        }
+        return b;
     }
 }
