6fb307a6786d8783ae21d0a164817b71b5404afa
krunalnagendra <147229701+krunalnagendra@users.noreply.github.com>
2025-05-22 17:49:13 +0530

    Time: 32 ms (98.73%), Space: 110.9 MB (10.13%) - LeetHub

--- 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.java
+++ 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.java
@@ -1,40 +1,31 @@
 class Solution {
-    public int maxRemoval(int[] nums, int[][] queries) {
+    public static int maxRemoval(int[] nums, int[][] queries) {
         int n = nums.length;
-        int m = queries.length;
-        int low = 0, high = m, answer = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (canMakeZeroArray(nums, queries, m - mid)) {
-                answer = mid;
-                low = mid + 1; 
-            } else {
-                high = mid - 1;
-            }
-        }
-        return answer;
-    }
-
-    private boolean canMakeZeroArray(int[] nums, int[][] queries, int usedQueries) {
-        int n = nums.length;
-        int[] diff = new int[n + 2];
-        for (int i = 0; i < usedQueries; i++) {
-            int l = queries[i][0];
-            int r = queries[i][1];
-            diff[l] += 1;
-            if (r + 1 < n) {
-                diff[r + 1] -= 1;
-            }
-        }
-        int[] coverage = new int[n];
-        int current = 0;
+        int[] endCount = new int[n];
+        for (int[] query : queries)
+            endCount[query[0]]++;
+        int[][] groupedEnds = new int[n][];
+        for (int i = 0; i < n; i++)
+            groupedEnds[i] = new int[endCount[i]];
+        for (int[] query : queries)
+            groupedEnds[query[0]][--endCount[query[0]]] = query[1];
+        int removedCount = 0;
+        Queue<Integer> activeEnds = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
-            current += diff[i];
-            coverage[i] = current;
-            if (coverage[i] < nums[i]) {
-                return false;
+            for (int end : groupedEnds[i])
+                activeEnds.offer(-end); 
+            int required = nums[i] - removedCount;
+            if (required > 0) {
+                if (required > activeEnds.size()) return -1;
+                removedCount += required;
+                while (required-- > 0) {
+                    int end = -activeEnds.poll();
+                    if (end < i) return -1;
+                    endCount[end]++;
+                }
             }
+            removedCount -= endCount[i];
         }
-        return true;
+        return activeEnds.size();
     }
 }
